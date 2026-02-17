16b9f322cc963e48d29363a7022d733f29596b02
Rahul Jangid <39289586+rahuljangid236@users.noreply.github.com>
2026-02-17 21:44:35 +0530

    Changes

--- website/components/VideoSection.tsx
+++ website/components/VideoSection.tsx
@@ -2,7 +2,7 @@ import { HeroVideoDialog } from "./ui/hero-video-dialog";
 
 const VideoSection = () => {
   return (
-    <div className="container m-auto relative bg-[linear-gradient(180deg,_rgba(169,163,194,0.2)_0%,_rgba(169,163,194,0.047)_100%)]">
+    <div className="container m-auto relative bg-[linear-gradient(180deg,rgba(169,163,194,0.2)_0%,rgba(169,163,194,0.047)_100%)]">
       <HeroVideoDialog
         className="block dark:hidden"
         animationStyle="from-center"
